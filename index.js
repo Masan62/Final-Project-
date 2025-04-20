6517759930ab7574444b414100c0e8d2218af522
Masan62 <razavim1@msu.edu>
2025-04-20 18:48:24 +0000

    Added txt and image

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add JavaScript below
+document.addEventListener("DOMContentLoaded", () => {
+    const workoutBtn = document.querySelector(".workout-btn");
+  
+    if (workoutBtn) {
+      workoutBtn.addEventListener("click", () => {
+        window.location.href = "workout.html"; // Navigate to second page
+      });
+    } else {
+      console.log("Workout button not found");
+    }
+  });
+  
